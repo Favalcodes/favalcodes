c67a39784c367c6439af22d73203b98e60afefe8
Favour Godwin Arua <favourvalg@gmail.com>
2020-08-05 02:47:12 +0100

    my portfolio

---
+++ index.php
@@ -0,0 +1,304 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./css/bootstrap-4.5.0-dist/css/bootstrap.min.css">
+
+    <!-- Font Awesome Cdn -->
+    <link rel="stylesheet" href="./css/font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="./css/ionicons-2.0.1/css/ionicons.min.css">
+
+    <!-- Aos Css -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <!-- Main Css -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>FavalCodes</title>
+</head>
+
+<body>
+    <main>
+        <header>
+            <!-- Navbar -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+                <a class="navbar-brand text-white w-20 h-10" href="#"><strong>FavalCodes</strong></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link text-white" href="index.html">Home <span
+                                    class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#about">About Me</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#service">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#contact">Contact Me</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- Header text -->
+            <div class="header-text text-white">
+                <h3>Hi <i class="ion-android-hand"></i><br>
+                    I am Favour Ezinne Arua<br>
+                    I am a web developer</h3>
+                <a href="#contact"><button class="btn bg-light text-dark mt-3"><strong>Contact Me</strong></button></a>
+            </div>
+        </header>
+
+        <div class="sections">
+            <!-- About section -->
+            <section class="about" id="about" data-aos="fade-up">
+                <div class="d-flex">
+                    <h4 class="pr-3">About Me</h4><img src="./images/Line 1.svg" alt="" class="img-svg3">
+                </div>
+                <p class="about-text text-align-left">
+                    I am a web developer with robust problem-solving skills with creating<br> and designing web
+                    applications in a test-driven environment,<br> I have experience with HTML, CSS, Javascript,
+                    Wordpress, Figma and Php.
+                </p>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="./images/_OLA6995.jpg" alt="" class="img-about">
+                    </div>
+                    <div class="col-md-4">
+                        <img src="./images/_OLA7710.jpg" alt="" class="img-about">
+                    </div>
+                    <div class="col-md-4">
+                        <img src="./images/_OLA7014.jpg" alt="" class="img-about">
+                    </div>
+                </div>
+            </section>
+
+            <!-- Service section -->
+            <section class="service pt-5" id="service" data-aos="fade-down">
+                <div class="row">
+                    <div class="col-md-8">
+
+                        <div class="d-flex">
+                            <h4 class="pr-3">My Services</h4><img src="./images/Line 1.svg" alt="" class="img-svg3">
+                        </div>
+                        <div class="row pl-3">
+                            <p class="about-text">
+                                I offer Web services, below are the services I offer:<br>
+                                <strong>Web design</strong><br>
+                                <strong>Web Development</strong><br>
+                                <strong>UI/UX Design</strong><br>
+                                <strong>Wordpress</strong>
+                            </p>
+                        </div>
+                        <div class="row pl-3">
+                            <img src="./images/web.png" alt="" class="service-img2">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="./images/darkfigma.png" alt="" class="service-img">
+                    </div>
+                </div>
+            </section>
+        </div>
+
+
+        <!-- Latest work -->
+        <section class="latest" id="latest" data-aos="fade-down">
+            <div class="d-flex">
+                <h4 class="pr-3">My Latest Work</h4><img src="./images/Line 1.svg" alt="" class="img-svg3">
+            </div>
+            <p class="about-text">
+                My latest works
+            </p>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text-tag">Panuel Bookstore</div>
+                    <img src="./images/my port.png" alt="" class="latest-img">
+
+                    <img src="./images/Line 1.svg" alt="" class="latest-svg">
+                    <div class="d-flex justify-content-between pl-3 pr-3">
+                        <div class="name">
+                            <p><strong>Panuel Bookstore</strong><br>
+                            A Wordpress site</p>
+                        </div>
+                        <div class="">
+                            <a href="https://panuelbookstore.com"><button class="btn">View</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="text-tag3">Panuel MFBank</div>
+                    <img src="./images/my port2.png" alt="" class="latest-img">
+
+                    <img src="./images/Line 1.svg" alt="" class="latest-svg">
+                    <div class="d-flex justify-content-between pl-3 pr-3">
+                        <div class="name">
+                            <p><strong>Panuel Microfinance Bank</strong></p>
+                        </div>
+                        <div class="">
+                        <a href="https://panuelmfbank.com"><button class="btn">View</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text-tag">Caveman Portfolio</div>
+                    <img src="./images/my port3.png" alt="" class="latest-img">
+
+                    <img src="./images/Line 1.svg" alt="" class="latest-svg">
+                    <div class="d-flex justify-content-between pl-3 pr-3">
+                        <div class="name">
+                            <p><strong>A Portfolio Template</strong></p>
+                        </div>
+                        <div class="">
+                        <a href="https://cavvy.netlify.app/"><button class="btn">View</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="text-tag3">Landing page</div>
+                    <img src="./images/screencapture-127-0-0-1-5500-index-html-2020-08-04-23_30_54.png" alt="" class="latest-img">
+
+                    <img src="./images/Line 1.svg" alt="" class="latest-svg">
+                    <div class="d-flex justify-content-between pl-3 pr-3">
+                        <div class="name">
+                            <p><strong>An App Landing page</strong></p>
+                        </div>
+                        <div class="">
+                        <a href="https://chat-landing.vercel.app/"><button class="btn">View</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="sections">
+
+            <!-- Contact Us -->
+            <section class="contact" id="contact" data-aos="zoom-out">
+                <div class="d-flex">
+                    <h4 class="pr-3">Contact Me</h4><img src="./images/Line 1.svg" alt="" class="img-svg3">
+                </div>
+                <div class="main-contact">
+                    <div class="row">
+                        <div class="col-md-4 justify-content-center">
+                            <i class="fa fa-map-marker" aria-hidden="true"></i><br>
+                            <p>Nigeria</p>
+                        </div>
+                        <div class="col-md-4 justify-content-center">
+                            <i class="fa fa-phone"></i>
+                            <p>+234 9061591694</p>
+                            <p>+234 7054528890</p>
+                        </div>
+                        <div class="col-md-4 justify-content-center">
+                            <i class="fa fa-envelope-o" aria-hidden="true"></i><br>
+                            <p>favourvalg@gmail.com</p>
+                        </div>
+                    </div>
+
+                    
+
+                    
+
+                    <form action="index.php" method="POST" class="form-group form">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label for="Name">Name:</label><br>
+                                <input type="text" class="form-control" name="name" required>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="Email">Email:</label><br>
+                                <input type="text" class="form-control" name="email" required>
+                            </div>
+                        </div>
+                        <div class="row textarea">
+                            <label for="comment">Message:</label>
+                            <textarea class="form-control" rows="5" name="message" required></textarea>
+                        </div>
+                        <input type="submit" name="submit" class="submit" value="Send">
+                    </form>
+                    <?php
+    if($_POST["submit"]) {
+        $recipent = "favourvalg@gmail.com";
+        $subject="Job contact";
+
+        $name=$_POST["name"];
+        $email=$_POST['email'];
+        $message=$_POST['message'];
+        $mailBody='Name: $name\n Email: $email\n \n $message';
+
+        mail($recipent, $subject, $mailBody, 'From: $name <$email>');
+
+        $thankYou = '<p>Thank you for contacting! Your message has been sent.</p>';
+    }
+?>
+
+<?=$thankYou ?>
+                </div>
+            </section>
+        </div>
+        <!-- footer -->
+        <div class="footer">
+            <div class="row footer-links">
+                <div class="col-md-6">
+                    <h5>FavalCodes</h5>
+                    <p>Nigeria</p>
+                </div>
+                <div class="col-md-6">
+                    <ul>
+                        <li class="list">
+                            <a href="#about">About Me</a>
+                        </li>
+                        <li class="list">
+                            <a href="#service">My Services</a>
+                        </li>
+                        <li class="list">
+                            <a href="#latest">My Latest Work</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between">
+                <div class="social-icons">
+                    <div class="d-flex justify-content-around">
+                        <a href="https://linkedin.com/in/favour-ezinne-arua-534696165"><i class="fa fa-linkedin"></i></a>
+                        <a href="https://twitter.com/godwinvalerie1"><i class="fa fa-twitter"></i></a>
+                        <a href="https://instagram.com/lady_that_codes/"><i class="fa fa-instagram"></i></a>
+                    </div>
+                </div>
+                <div class="foto">
+                    <h5>FavalCodes</h5>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="./js/main.js"></script>
+</body>
+
+</html>
